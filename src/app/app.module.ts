2eb2282113154cab3a10cf45a68fc8b34d4cc7ec
chkramesh <chkramesh@gmail.com>
2019-05-05 07:16:55 -0400

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+import { HttpModule, Http } from '@angular/http';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
+import { SearchComponent } from './search/search.component';
+// import { OthersComponent } from './others/others.component';
+// import { AddressComponent } from './address/address.component';
+
+import 'hammerjs';
+
+// import { MatTabsModule } from '@angular/material';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'search', component: SearchComponent },
+  // { path: 'others', component: OthersComponent },
+  // { path: 'address', component: AddressComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    FormsModule,
+    // MatTabsModule,
+    HttpModule, 
+    FormsModule, ReactiveFormsModule,
+    NgxWebstorageModule.forRoot(),
+    MaterialModule, FlexLayoutModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [
+    AppComponent, 
+    HelloComponent, 
+    MembersComponent, 
+    HomeComponent,
+    SearchComponent, 
+    // OthersComponent,
+    // AddressComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
