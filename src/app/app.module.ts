1b420edc615e5a3b61959eaed0eae81f567b8983
chkramesh <chkramesh@gmail.com>
2019-05-05 07:19:50 -0400

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
 import { HomeComponent } from './home/home.component';
 import { MembersComponent } from './members/members.component';
 import { SearchComponent } from './search/search.component';
