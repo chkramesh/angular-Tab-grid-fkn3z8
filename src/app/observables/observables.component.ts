1070babc3f36d658336813621f71d723c0918d89
chkramesh <chkramesh@gmail.com>
2019-05-09 11:59:36 -0400

    1 file modified

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ReactiveFormsModule, FormControl, FormsModule } from "@angular/forms";
+
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
@@ -11,5 +15,26 @@ export class ObservablesComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // private loading: boolean = false;
+  // private results: Observable<SearchItem[]>;
+  // private searchField: FormControl;
+
+  // constructor(private itunes: SearchService) {}
+
+  // ngOnInit() {
+  //   this.searchField = new FormControl();
+  //   this.results = this.searchField.valueChanges.pipe(
+  //     debounceTime(400),
+  //     distinctUntilChanged(),
+  //     tap(_ => (this.loading = true)),
+  //     switchMap(term => this.itunes.search(term)),
+  //     tap(_ => (this.loading = false))
+  //   );
+  // }
+
+  // doSearch(term: string) {
+  //   this.itunes.search(term);
+  // }
 
 }
