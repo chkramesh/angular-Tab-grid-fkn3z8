a36fa8f7d041e16e9a2b70539dcdbabab1a4742b
chkramesh <chkramesh@gmail.com>
2019-05-09 11:07:07 -0400

    6 files added

---
+++ src/app/model/index.ts
@@ -0,0 +1,4 @@
+export * from './student';
+
+// export * from './user';
+// export * from './search-item';
---
+++ src/app/service/search.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+import { Student } from '../model';
+
+
+// import { User } from '../models';
+// import { SearchItem } from '../search-items';
+
+// import { User } from '../models';
+
+// import { SearchItem } from './models/search-item';
+// import { SearchItemModel } from './model';
+// import { SearchItem } from '../model';
+
+// import { MemberModel } from './members/member.model'
+
+@Injectable()
+export class SearchService {
+
+   apiRoot: string = "https://itunes.apple.com/search";
+  constructor(private http: HttpClient) {}
+
+  search(term: string): Observable<SearchItem[]> {
+    let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+    return this.http.get(apiURL).pipe(
+      map(res => {
+        return res.results.map(item => {
+          return new SearchItem(
+            item.trackName,
+            item.artistName,
+            item.trackViewUrl,
+            item.artworkUrl30,
+            item.artistId
+          );
+        });
+      })
+    );
+  }
+
+}
