fcd66fb2d3539b566da1e4f51c57d3033fd39bf3
chkramesh <chkramesh@gmail.com>
2019-05-09 11:53:16 -0400

    1 file modified

--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import { Student } from '../model';
 import { User } from '../models';
+import { SearchItem } from '../models';
 
 // import { User } from '../models';
 // import { SearchItem } from '../search-items';
