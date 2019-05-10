86a2ca81975c0c428c2fb9fbbed52161c534074c
chkramesh <chkramesh@gmail.com>
2019-05-10 10:11:22 -0400

    2 files modified

--- src/app/service/common/common-app.service.ts
+++ src/app/service/common/common-app.service.ts
@@ -5,8 +5,9 @@ import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
-// import { Student } from '../model';
 import { User } from '.../models';
+// import { Student } from '../model';
+// import { User } from '.../models';
 // import { SearchItem } from '../models';
 
 @Injectable()
