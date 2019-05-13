431007003292b4c1c78c81a887dfa04d20a02923
chkramesh <chkramesh@gmail.com>
2019-05-13 11:17:02 -0400

    1 file modified

--- src/app/helper/error-interceptor.ts
+++ src/app/helper/error-interceptor.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-
-
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
