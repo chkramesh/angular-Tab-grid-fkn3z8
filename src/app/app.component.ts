2ac3463b6a55d9fe8c6ec0c52b20f4434b32864e
chkramesh <chkramesh@gmail.com>
2019-05-17 12:34:14 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ReactiveFormsModule, FormControl, FormsModule } from "@angular/forms";
+
+import { CommonAppService } from './service/common/common-app.service';
+import { User } from './models';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit {
   name = 'Angular 7 reactive form';
   userName = 'Test User';
+
+   private loading: boolean = false;
+  // private results: Observable<SearchItem[]>;
+  private searchField: FormControl;
+
+  // private userResults: Observable<User[]>;
+  public userResults: Observable<User[]>;
+  // userResults: Observable<User[]>;
+
+  constructor(private commonappservice: CommonAppService) { }
+
+  ngOnInit() {
+    this.getUser(1);
+    this.fetchUserRecord(1);
+
+  }
+
+
+  getUser(userId:number) {
+    console.log('Observable Component getUserOnLoad id = ' + userId);
+    this.loading = true;
+    this.commonappservice.getUser(userId).subscribe( data => {
+      this.loading = false;
+      this.userResults = data;
+      // this.processResults();
+      console.log(' 2 this.userResults = ' + this.userResults);
+      // const record = this.userResults.find(obj => obj[this.id] === userId);
+    });
+  }
+
+  processResults() {
+    // Do some stuff with your results, this.Result is set now
+    console.log(' 3 this.userResults = ' + this.userResults);
+  }
+
+  public fetchUserRecord(userId:number) {
+    // Display the data retrieved from the data model to the form model.
+    this.commonappservice.getRecordById(userId)
+        .subscribe(data => {
+            // this.fillForm(data);
+            console.log(' 3 this.userResults data = ' +data.id + ' firstName = ' + data.firstName);
+          },
+          // (err: HttpErrorResponse) => {
+          //   console.log(err.error);
+          //   console.log(err.message);
+          //   this.handleError(err);
+          // }
+          );
+  }
 }
 
 
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -16,52 +16,53 @@ export class FormComponent implements OnInit {
 
   // userName = 'Test Form User';
 
-  private loading: boolean = false;
-  // private results: Observable<SearchItem[]>;
-  private searchField: FormControl;
+  // private loading: boolean = false;
+  // // private results: Observable<SearchItem[]>;
+  // private searchField: FormControl;
 
-  // private userResults: Observable<User[]>;
-  public userResults: Observable<User[]>;
-  // userResults: Observable<User[]>;
+  // // private userResults: Observable<User[]>;
+  // public userResults: Observable<User[]>;
+  // // userResults: Observable<User[]>;
+  // constructor(private commonappservice: CommonAppService) { }
 
-  constructor(private commonappservice: CommonAppService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getUser(1);
-    this.fetchUserRecord(1);
+    // this.getUser(1);
+    // this.fetchUserRecord(1);
 
   }
 
-  getUser(userId:number) {
-    console.log('Observable Component getUserOnLoad id = ' + userId);
-    this.loading = true;
-    this.commonappservice.getUser(userId).subscribe( data => {
-      this.loading = false;
-      this.userResults = data;
-      // this.processResults();
-      console.log(' 2 this.userResults = ' + this.userResults);
-      // const record = this.userResults.find(obj => obj[this.id] === userId);
-    });
-  }
+  // getUser(userId:number) {
+  //   console.log('Observable Component getUserOnLoad id = ' + userId);
+  //   this.loading = true;
+  //   this.commonappservice.getUser(userId).subscribe( data => {
+  //     this.loading = false;
+  //     this.userResults = data;
+  //     // this.processResults();
+  //     console.log(' 2 this.userResults = ' + this.userResults);
+  //     // const record = this.userResults.find(obj => obj[this.id] === userId);
+  //   });
+  // }
 
-  processResults() {
-    // Do some stuff with your results, this.Result is set now
-    console.log(' 3 this.userResults = ' + this.userResults);
-  }
+  // processResults() {
+  //   // Do some stuff with your results, this.Result is set now
+  //   console.log(' 3 this.userResults = ' + this.userResults);
+  // }
 
-  public fetchUserRecord(userId:number) {
-    // Display the data retrieved from the data model to the form model.
-    this.commonappservice.getRecordById(userId)
-        .subscribe(data => {
-            // this.fillForm(data);
-            console.log(' 3 this.userResults data = ' +data.id + ' firstName = ' + data.firstName);
-          },
-          // (err: HttpErrorResponse) => {
-          //   console.log(err.error);
-          //   console.log(err.message);
-          //   this.handleError(err);
-          // }
-          );
-  }
+  // public fetchUserRecord(userId:number) {
+  //   // Display the data retrieved from the data model to the form model.
+  //   this.commonappservice.getRecordById(userId)
+  //       .subscribe(data => {
+  //           // this.fillForm(data);
+  //           console.log(' 3 this.userResults data = ' +data.id + ' firstName = ' + data.firstName);
+  //         },
+  //         // (err: HttpErrorResponse) => {
+  //         //   console.log(err.error);
+  //         //   console.log(err.message);
+  //         //   this.handleError(err);
+  //         // }
+  //         );
+  // }
 
 }
