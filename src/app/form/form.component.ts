af37fbcc9916a2ab62ec84a411eda9d9221dd329
chkramesh <chkramesh@gmail.com>
2019-05-17 10:31:42 -0400

    1 file modified

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ReactiveFormsModule, FormControl, FormsModule } from "@angular/forms";
+
+import { CommonAppService } from '../service/common/common-app.service';
+import { User } from '../models';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  private loading: boolean = false;
+  // private results: Observable<SearchItem[]>;
+  private searchField: FormControl;
+
+  // private userResults: Observable<User[]>;
+  public userResults: Observable<User[]>;
+  // userResults: Observable<User[]>;
+
+  constructor(private commonappservice: CommonAppService) { }
 
   ngOnInit() {
+    this.getUser(1);
+    this.fetchUserRecord(1);
+
+  }
+
+  getUser(userId:number) {
+    console.log('Observable Component getUserOnLoad id = ' + userId);
+    this.loading = true;
+    this.commonappservice.getUser(userId).subscribe( data => {
+      this.loading = false;
+      this.userResults = data;
+      // this.processResults();
+      console.log(' 2 this.userResults = ' + this.userResults);
+      // const record = this.userResults.find(obj => obj[this.id] === userId);
+    });
+  }
+
+  processResults() {
+    // Do some stuff with your results, this.Result is set now
+    console.log(' 3 this.userResults = ' + this.userResults);
+  }
+
+  public fetchUserRecord(userId:number) {
+    // Display the data retrieved from the data model to the form model.
+    this.commonappservice.getRecordById(userId)
+        .subscribe(data => {
+            // this.fillForm(data);
+            console.log(' 3 this.userResults data = ' +data.id + ' firstName = ' + data.firstName);
+          },
+          // (err: HttpErrorResponse) => {
+          //   console.log(err.error);
+          //   console.log(err.message);
+          //   this.handleError(err);
+          // }
+          );
   }
 
 }
